889c85d775ca3f4d788c67233cb569360b3d62a5
T-REX911 <163975890+T-REX911@users.noreply.github.com>
2025-05-06 22:53:42 +0530

    main PC values - 4

--- src/Creational_Design_Patterns/Factory_pattern/main.java
+++ src/Creational_Design_Patterns/Factory_pattern/main.java
@@ -3,7 +3,7 @@ package Creational_Design_Patterns.Factory_pattern;
 public class main {
 
     public static void main(String args[]){
-        Computer PC = ComputerFactory.getComputer("PC","4Gb","2Tb","2.4GHz");
+        Computer PC = ComputerFactory.getComputer("PC","4Gb","4Tb","2.4GHz");
         Computer Server = ComputerFactory.getComputer("Server","48Gb","100Tb","5.0GHz");
 
         System.out.println("Factory PC Config::"+ PC);
