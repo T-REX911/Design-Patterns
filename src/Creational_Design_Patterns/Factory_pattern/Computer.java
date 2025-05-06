e5ef8c7e0b5deea83a2588735d7a75e356794fbb
T-REX911 <163975890+T-REX911@users.noreply.github.com>
2025-05-06 22:44:56 +0530

    Factory pattern

---
+++ src/Creational_Design_Patterns/Factory_pattern/Computer.java
@@ -0,0 +1,13 @@
+package Creational_Design_Patterns.Factory_pattern;
+
+public abstract class Computer {
+
+    public abstract String getRAM();
+    public abstract String getHDD();
+    public abstract String getCPU();
+
+    @Override
+    public String toString(){
+        return "RAM= "+this.getRAM()+", HDD="+this.getHDD()+", CPU="+this.getCPU();
+    }
+}
--- src/Creational_Design_Patterns/Factory_pattern/main.java
+++ src/Creational_Design_Patterns/Factory_pattern/main.java
@@ -1,4 +1,16 @@
 package Creational_Design_Patterns.Factory_pattern;
 
 public class main {
+
+    public static void main(String args[]){
+        Computer PC = ComputerFactory.getComputer("PC","2Gb","1Tb","1.2GHz");
+        Computer Server = ComputerFactory.getComputer("Server","24Gb","100Tb","5.0GHz");
+
+        System.out.println("Factory PC Config::"+ PC);
+        System.out.println("Factory Server Config::"+Server);
+    }
+
+
+
+
 }
